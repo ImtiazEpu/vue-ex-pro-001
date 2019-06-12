59ad04328004b39ebf638157bafa6b638527fb33
Imtiaz Epu <imtiazepu@programmer.net>
2019-06-12 19:52:13 +0600

    code update

---
+++ src/components/eventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 v-if @click="handler(arg, event)"></h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: value
+    }
+  }
+}
+</script>
+
+<style></style>
